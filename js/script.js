bea194d12620fe4c3bc2ea3da77c25bbad320298
mahdi bashiri <mahdi94250@gmail.com>
2022-02-09 00:28:10 +0330

    fix bug

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
+
+// OPEN & CLOSE MENU IN MOBILE 
 const toggler = document.querySelector('.nav__toggler')
 const nav = document.querySelector('.nav')
 toggler.addEventListener("click", e => {
   nav.classList.toggle('nav__expanded')
 })
 
+// TAB IN SERVICES 
 const tabs = document.querySelectorAll('.nav__tab')
 const tabContents = document.querySelectorAll('.tab__content')
 tabs.forEach(tab => {
@@ -18,6 +21,7 @@ tabs.forEach(tab => {
   })
 });
 
+// OPEN & CLOSE ACCORDION 
 const accordionTab = document.querySelectorAll('.acordian__header')
 accordionTab.forEach(tach => {
   tach.addEventListener("click", e => {
@@ -25,6 +29,8 @@ accordionTab.forEach(tach => {
   })
 })
 
+
+// SHOW & HIDE BTN SCROLL TO TOP 
 window.onscroll = function () { isScrollTop() }
 function isScrollTop() {
   const btnScrollTop = document.querySelector('.scroll-top')
